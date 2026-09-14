228413cdca08fd1d817ddf68525fddf2aa9bf3f2
andresmagicdev009 <andres.rodriguezso09@gmail.com>
2026-09-13 20:42:21 -0500

    Demo Hello World with RestController Bean

---
+++ project_2/demo/src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController 
+@RequestMapping("/api/v1/test")
+public class HelloController {
+    @GetMapping("hello")
+    public String helloWord(@RequestParam String name) {
+        String message = String.format("Hello %s!", name);
+        return message;
+    }
+    
+}
