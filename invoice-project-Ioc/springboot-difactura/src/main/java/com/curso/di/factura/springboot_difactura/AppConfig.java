36d2033b7f8a05452b6e163edb2aa57ed399e472
andresmagicdev009 <andres.rodriguezso09@gmail.com>
2026-09-17 21:25:56 -0500

    WIP Invoice

---
+++ invoice-project-Ioc/springboot-difactura/src/main/java/com/curso/di/factura/springboot_difactura/AppConfig.java
@@ -0,0 +1,24 @@
+package com.curso.di.factura.springboot_difactura;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.curso.di.factura.springboot_difactura.models.Item;
+import com.curso.di.factura.springboot_difactura.models.Product;
+
+@Configuration 
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+    
+    @Bean
+    List<Item> itemInvoice(){
+        Product product1 = new Product("HeadhPhone JBL", 100);
+        Product product2 = new Product("Bicycle", 200);
+        return Arrays.asList(new Item(product1, 5), new Item(product2, 10));
+    }
+    
+}
--- invoice-project-Ioc/springboot-difactura/src/main/java/com/curso/di/factura/springboot_difactura/models/Client.java
+++ invoice-project-Ioc/springboot-difactura/src/main/java/com/curso/di/factura/springboot_difactura/models/Client.java
@@ -1,7 +1,14 @@
 package com.curso.di.factura.springboot_difactura.models;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component 
 public class Client {
+
+    @Value("${client.name}")
     private String name;
+    @Value("${client.lastname}")
     private String lastName;
 
     public String getName() {
